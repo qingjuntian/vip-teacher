5d50ea27adc22de902c3f6adb82abb056b0503bc
Qing Jun <qingjun@ahi-fintech.com>
2018-03-12 21:02:06 +0800

    format update

--- vip_teacher.py
+++ vip_teacher.py
@@ -13,39 +13,55 @@ from docx.shared import Inches
 def read_words(file):
     document = Document(file)
     words = []
+
     for p in document.paragraphs:
         words.extend(p.text.split(' '))
-        print words
     return words
 
-def output_words(outfile, words):
-    doc = Document(outfile)
+def output_words_with_paragraph(outfile, words):
+    doc = Document()
     global title
-    doc.add_heading(title, 0)
+    #doc.add_heading(title, 0)
     line_words = 0
-    line=''
+    line_pinyin=''
+    line_to_input=''
+    char_num = 0
+    first = True
     for word in words:
+        char_num += len(word)
         py = pinyin(word)
-        line += u' '.join(w[0] for w in py)
-        line += u'_' * len(py) * 2
-        line += u' ' * 2
-        print line
-        line_words += 1
+        word_pinyin = u' '.join(w[0] for w in py)
+        word_pinyin_number = len(word_pinyin)
 
-        if line_words == 5:
+        append_whitespace_number = (len(word) * space_num_per_char - word_pinyin_number * 3 / 2 + 8 / len(word)) / 2
+
+        line_pinyin += u' ' * append_whitespace_number + word_pinyin + u' ' * append_whitespace_number
+
+        line_to_input += u'' if first else u'  '
+        line_to_input += u'(' + (u' '* len(py) * space_num_per_char) + ')'
+
+        line_words += 1
+        if first: first = False
+
+        if char_num >= 14:
             #print repr(line)
-            doc.add_paragraph(line)
-            line = ''
-            line_words = 0
-    doc.add_paragraph(line)
+            doc.add_paragraph(line_pinyin)
+            doc
+            doc.add_paragraph(line_to_input)
+            line_pinyin = ''
+            line_to_input = ''
+            char_num = 0
+            first = True
+    doc.add_paragraph(line_pinyin)
+    doc.add_paragraph(line_to_input)
     doc.save(outfile)
 
 title = u"小学五年级第一单元看拼音写汉字"
-space_num_per_char=6
+space_num_per_char=8
 
 if __name__ == "__main__":
     words = read_words(sys.argv[1])
-    output_words(sys.argv[2], words)
+    output_words_with_paragraph(sys.argv[2], words)
 
 
 #
